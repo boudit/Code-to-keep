0ef27b3cae220c18dd565bd2d6346ca23bed2f11
tibo <th.boudier@gmail.com>
2017-06-15 14:44:02 +0200

    Add a TypeExtensions

---
+++ Extensions/Extensions/Primitives/TypeExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Extensions.Primitives
+{
+    using System;
+    using System.Linq;
+
+    public static class TypeExtensions
+    {
+        public static bool IsSubclassOfGenericType(this Type toCheck, Type generic, params Type[] genericArguments)
+        {
+            while (toCheck != null && toCheck != typeof(object))
+            {
+                var current = toCheck.IsGenericType ? toCheck.GetGenericTypeDefinition() : toCheck;
+                if (generic == current)
+                {
+                    if (genericArguments == null || !genericArguments.Any() || current.GenericTypeArguments.SequenceEqual(genericArguments))
+                    {
+                        return true;
+                    }
+                }
+
+                toCheck = toCheck.BaseType;
+            }
+
+            return false;
+        }
+    }
+}
